582ed342dd1ff5bbba660737ef44d99a666da48d
Mateusz Starzycki <mateusz.starzycki@ultrahaptics.com>
2015-02-18 17:48:08 +0000

    zadania opt, inz wym

--- game/main.cpp
+++ game/main.cpp
@@ -1,9 +1,14 @@
-#include "main.h"
-
 int main(){
-	initscr();
-	printw("Hello World !!!");
-	refresh();	
-	getch();
-	endwin();
-}
+
+#include "../../sdl2base/GLWindow.h"
+
+class ExampleWindow : public GLWindow
+{
+    public:
+       using GLWindow::checkEvents;
+       ExampleWindow(const char *ititle, size_t width, size_t height, Uint32 windowflags);
+       ~ExampleWindow();
+       int checkEvents(Leap::Controller &controller);
+ };
+ 
+ #endif }
