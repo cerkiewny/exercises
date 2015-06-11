8c63135d637e5b870bbff2cac6f5bab41523739d
Mateusz Starzycki <mateusz.starzycki@ultrahaptics.com>
2015-06-11 11:22:49 +0100

    opengl try

---
+++ opengl/1/main.cpp
@@ -0,0 +1,18 @@
+
+
+
+int main(){
+  glClearColor(0.0f, 0.0f, 0.0f);
+  glClear(GL_COLOR_BUFFER_BIT);
+  glUseProgram(theProgram);
+  glBindBuffer(GL_ARRAY_BUFFER, positionBufferObject);
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, 0);
+
+  glDrawArrays(GL_TRIANGLES, 0, 3);
+
+  glDisableVertexAttribArray(0);
+  glUseProgram(0);
+
+  glutSwapBuffers();
+}
