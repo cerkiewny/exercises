5c25fc359f24266fe7997b0c6b2337fb6715156f
Mateusz Starzycki <mateusz.starzycki@ultrahaptics.com>
2015-07-02 23:03:14 +0100

    dane do raportu

--- studia/systemyczasu/zad4/avarage.py
+++ studia/systemyczasu/zad4/avarage.py
@@ -3,8 +3,22 @@ for arg in sys.argv[1:]:
     i = 0
     f = open(arg, 'r')
     suma = 0
-    for line in f.readlines():
+    minim = 100.0
+    maxim = 0.0
+    lines = f.readlines()
+    for line in lines:
         i += 1
         suma += float(line)
+        if float(line) > maxim:
+            maxim = float(line)
+        if float(line) < minim:
+            minim = float(line)
+    step = (maxim - minim) / 100.0
+
+    for line in lines:
+        print int((float(line) - minim) / step)
+
     if i != 0:
         print (suma / i)
+        print minim
+        print maxim
